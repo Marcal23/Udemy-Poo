a8dbb8f369939acdfe670ab8e333843dcc4d1462
Marcos marçal <marcosvmarcal23@gmail.com>
2020-06-11 20:21:02 -0300

    Primeiro Commit!

---
+++ CalcularSalario/CalcularSalario/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿namespace CalcularSalario
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * (porcentagem / 100));
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + SalarioLiquido().ToString("F2");
+        }
+    }
+}
---
+++ CalcularSalario/CalcularSalario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalcularSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            double porcentagem;
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Funcionario: " + funcionario);
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            porcentagem = Convert.ToDouble(Console.ReadLine());
+            funcionario.AumentarSalario(porcentagem);
+            Console.WriteLine();
+
+            Console.Write("Dados atualizados: " + funcionario);
+
+        }
+    }
+}
