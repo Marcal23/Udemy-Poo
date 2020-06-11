bec8e8f276402d9b906921c6432b3125af20606c
Marcos marçal <marcosvmarcal23@gmail.com>
2020-06-11 20:55:54 -0300

    Commit!

---
+++ BoletimAluno/BoletimAluno/Aluno.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoletimAluno
+{
+    public class Aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double SomaNotas()
+        {
+            return nota1 + nota2 + nota3;
+        }
+        public bool Aprovado()
+        {
+            if (SomaNotas() >= 60)
+                return true;
+            else
+                return false;
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+                return 0.0;
+            else
+                return 60 - SomaNotas();
+        }
+    }
+}
---
+++ BoletimAluno/BoletimAluno/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BoletimAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            Console.Write("Nome do aluno: ");
+            aluno.nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.nota1 = Convert.ToInt32(Console.ReadLine());
+            aluno.nota2 = Convert.ToInt32(Console.ReadLine());
+            aluno.nota3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("NOTA FINAL = "+aluno.SomaNotas().ToString("F2")+"\n");
+            if (aluno.Aprovado())
+                Console.WriteLine("APROVADO");
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "+ aluno.NotaRestante().ToString("F2") + " PONTOS");
+            }
+        }
+    }
+}
