1b99433b90b8e5a46ff715dfd7bbc32c06ad2345
Marcos marçal <marcosvmarcal23@gmail.com>
2020-06-11 19:29:03 -0300

    Primero Commit!

---
+++ CalcularTriangulo/CalcularTriangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalcularRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+       
+            Console.WriteLine("Digite a largura e altura do retângulo: ");
+            retangulo.Largura = Convert.ToDouble(Console.ReadLine());
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("AREA = " + retangulo.Area().ToString("F2"));
+            Console.WriteLine("PERÍMETRO = " + retangulo.Perimetro().ToString("F2"));
+            Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2"));
+        }
+    }
+}
